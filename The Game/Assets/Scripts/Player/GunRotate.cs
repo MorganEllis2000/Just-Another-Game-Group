357981e914d964f42228961a560b0e1bfcd56cfa
Morgan Ellis <115263942+MorganEllis2000@users.noreply.github.com>
2022-10-27 00:01:36 +0100

    Creating animations for movement and idle
    Create animations for walking in 4 directions as well as idle states based of whether the player is facing forward or backwards.

---
+++ The Game/Assets/Scripts/Player/GunRotate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotate : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void FixedUpdate() {
+        Vector3 diffrence = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diffrence.Normalize();
+
+        float rotaionZ = Mathf.Atan2(diffrence.y, diffrence.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotaionZ);
+
+        if(transform.eulerAngles.z >= 0 && transform.eulerAngles.z <= 89) {
+            controller.SetGunDirection(GunDirection.NE);
+            controller.SetPlayerDirection(PlayerDirection.BACK);
+        }
+        if (transform.eulerAngles.z >= 180 && transform.eulerAngles.z <= 269) {
+            controller.SetGunDirection(GunDirection.SW);
+            controller.SetPlayerDirection(PlayerDirection.FRONT);
+        }
+        if (transform.eulerAngles.z >= 89 && transform.eulerAngles.z <= 179) {
+            controller.SetGunDirection(GunDirection.NW);
+            controller.SetPlayerDirection(PlayerDirection.BACK);
+        }
+        if (transform.eulerAngles.z >= 269 && transform.eulerAngles.z <= 360) {
+            controller.SetGunDirection(GunDirection.SE);
+            controller.SetPlayerDirection(PlayerDirection.FRONT);
+        }
+    }
+}
--- The Game/Assets/Scripts/Player/PlayerController.cs
+++ The Game/Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GunDirection {
+    NE,
+    NW,
+    SW,
+    SE,
+    NONE
+}
+public enum PlayerDirection {
+    FRONT,
+    BACK
+}
+
 public class PlayerController : MonoBehaviour
 {
-    
-    public enum PlayerDirection {
-        Left, 
-        Right, 
-        Up, 
-        Down
-    }
-
-    public PlayerDirection Direction;
+    protected PlayerDirection playerDirection;
+    protected GunDirection gunDirection;
 
     private float _Vertical;
     private float _Horizontal;
@@ -25,11 +30,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] protected float f_RunSpeed = 0.0f;
 
     protected Rigidbody2D rigidBody2D;
-    public SpriteRenderer spriteRenderer;
+    protected SpriteRenderer spriteRenderer;
+    protected Animator animator;
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,45 +45,95 @@ public class PlayerController : MonoBehaviour
     {
         _Horizontal = Input.GetAxisRaw("Horizontal");
         _Vertical = Input.GetAxisRaw("Vertical");
+
+        Debug.Log((int)playerDirection);
     }
 
     private void FixedUpdate() {
-        if (_Horizontal != 0 && _Vertical != 0) {
+        if (_Horizontal != 0 || _Vertical != 0) {
+            animator.SetBool("IsRunning", true);
+        } else {
+            SetGunDirection(GunDirection.NONE);
+            animator.SetBool("IsRunning", false);
+        }
 
+        
+
+        if (_Horizontal != 0 && _Vertical != 0) {
             _Horizontal *= _moveLimiter;
             _Vertical *= _moveLimiter;
         }
 
-        if(_Horizontal == 1) {
-            Direction = PlayerDirection.Right;
-        } else if (_Horizontal == -1) {
-            Direction = PlayerDirection.Left;
-        } else if (_Vertical == 1) {
-            Direction = PlayerDirection.Up;
-        } else if (_Vertical == -1) {
-            Direction = PlayerDirection.Down;
-        }
+        //if(_Horizontal == 1) {
+        //    Direction = PlayerDirection.Right;
+        //} else if (_Horizontal == -1) {
+        //    Direction = PlayerDirection.Left;
+        //} else if (_Vertical == 1) {
+        //    Direction = PlayerDirection.Up;
+        //} else if (_Vertical == -1) {
+        //    Direction = PlayerDirection.Down;
+        //}
 
         rigidBody2D.velocity = new Vector2(_Horizontal * f_RunSpeed, _Vertical * f_RunSpeed);
         ChangePlayerSprite();
     }
 
     public void ChangePlayerSprite() {
-        switch (Direction) {
-            case PlayerDirection.Left:
-                spriteRenderer.color = Color.red;
+        UpdateGunDirection();
+        UpdatePlayerDirection();
+    }
+
+    public void UpdateGunDirection() {
+        switch (gunDirection) {
+            case GunDirection.SE:
+                //spriteRenderer.color = Color.red;
+                spriteRenderer.flipX = false;
+                animator.SetInteger("SetGunDirection", (int)gunDirection);
                 return;
-            case PlayerDirection.Right:
-                spriteRenderer.color = Color.blue;
+            case GunDirection.NE:
+                //spriteRenderer.color = Color.blue;
+                spriteRenderer.flipX = false;
+                animator.SetInteger("SetGunDirection", (int)gunDirection);
                 return;
-            case PlayerDirection.Up:
-                spriteRenderer.color = Color.yellow;
+            case GunDirection.NW:
+                //spriteRenderer.color = Color.yellow;
+                spriteRenderer.flipX = true;
+                animator.SetInteger("SetGunDirection", (int)gunDirection);
                 return;
-            case PlayerDirection.Down:
-                spriteRenderer.color = Color.green;
+            case GunDirection.SW:
+                //spriteRenderer.color = Color.green;
+                spriteRenderer.flipX = true;
+                animator.SetInteger("SetGunDirection", (int)gunDirection);
+                return;
+            case GunDirection.NONE:
+                animator.SetInteger("SetGunDirection", (int)gunDirection);
                 return;
             default:
                 break;
         }
+    }
+
+    public void UpdatePlayerDirection() {
+        switch (playerDirection) {
+            case PlayerDirection.FRONT:
+                animator.SetInteger("SetPlayerDirection", (int)playerDirection);
+                return;
+            case PlayerDirection.BACK:
+                animator.SetInteger("SetPlayerDirection", (int)playerDirection);
+                return;
+            default:
+                break;
+
+        }
+    }
+
+
+
+    public PlayerDirection SetPlayerDirection(PlayerDirection direction) {
+        return playerDirection = direction;
+    }
+
+    public GunDirection SetGunDirection(GunDirection direction) {
+        return gunDirection = direction;
     }
 }
