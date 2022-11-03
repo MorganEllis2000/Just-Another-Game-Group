bdd9b8fc9fb2cabffd2e1f321b60838b3e3bf2a2
Morgan Ellis <morganellisonline@gmail.com>
2022-11-03 17:35:38 +0000

    Adding post processing package as well as the AI scripts for implemntation

--- The Game/Assets/Scripts/BehaviourTrees/Composites/Selector.cs
+++ The Game/Assets/Scripts/BehaviourTrees/Composites/Selector.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Selector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Selector : Node {
+    protected List<Node> nodes = new List<Node>();
+
+    public Selector(List<Node> nodes) {
+        this.nodes = nodes;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override NodeState Evaluate() {
+        foreach(var node in nodes) {
+            switch (node.Evaluate()) {
+                case NodeState.RUNNING:
+                    _nodeState = NodeState.RUNNING;
+                    return _nodeState;
+                case NodeState.SUCCESS:
+                    _nodeState = NodeState.SUCCESS;
+                    return _nodeState;
+                case NodeState.FAILURE:
+                    break;
+                default:
+                    break;
+            }
+        }
+        _nodeState = NodeState.FAILURE;
+        return _nodeState;
     }
 }
+
--- The Game/Assets/Scripts/BehaviourTrees/Nodes/Node.cs
+++ The Game/Assets/Scripts/BehaviourTrees/Nodes/Node.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BehaviourTrees {
-    [System.Serializable]
-    public abstract class Node {
-        protected NodeState _nodeState;
+[System.Serializable]
+public abstract class Node {
+    protected NodeState _nodeState;
 
-        public NodeState nodeState { get { return _nodeState; } } // get the current state of our node
+    public NodeState nodeState { get { return _nodeState; } } // get the current state of our node
 
-        public abstract NodeState Evaluate();
-    }
+    public abstract NodeState Evaluate();
+}
 
-    public enum NodeState {
-        RUNNING,
-        SUCCESS,
-        FAILURE,
-    }
+public enum NodeState {
+    RUNNING,
+    SUCCESS,
+    FAILURE,
 }
