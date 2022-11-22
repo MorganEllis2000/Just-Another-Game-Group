0a3f85d1915c07bc8f6fa59041ea12b9ea03d284
Morgan Ellis <115263942+MorganEllis2000@users.noreply.github.com>
2022-11-22 16:53:30 +0000

    Updating rock with close range attack sprite and fade in/out for dungeons

--- The Game/Assets/Scripts/Player/PlayerController.cs
+++ The Game/Assets/Scripts/Player/PlayerController.cs
@@ -44,6 +44,8 @@ public class PlayerController : MonoBehaviour
     protected SpriteRenderer spriteRenderer;
     protected Animator animator;
 
+    
+
     // DASH VARIABLES
 
     protected bool canDash = true;
@@ -78,38 +80,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isDashing == true) {
-            return;
-        }
+        if(Health > 0) {
+            if (isDashing == true) {
+                return;
+            }
 
-        _Horizontal = Input.GetAxisRaw("Horizontal");
-        _Vertical = Input.GetAxisRaw("Vertical");
+            _Horizontal = Input.GetAxisRaw("Horizontal");
+            _Vertical = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(KeyCode.LeftShift) && canDash == true) {
-            StartCoroutine(Dash());
+            if (Input.GetKey(KeyCode.LeftShift) && canDash == true) {
+                StartCoroutine(Dash());
+            }
         }
     }
 
     private void FixedUpdate() {
-        if(isDashing == true) {
-            return;
-        }
 
-        if (_Horizontal != 0 || _Vertical != 0) {
-            animator.SetBool("IsRunning", true);
+        if (Health > 0) {
+            if (isDashing == true) {
+                return;
+            }
+
+            if (_Horizontal != 0 || _Vertical != 0) {
+                animator.SetBool("IsRunning", true);
+            } else {
+                SetGunDirection(GunDirection.NONE);
+                animator.SetBool("IsRunning", false);
+            }
+
+            if (_Horizontal != 0 && _Vertical != 0) {
+                _Horizontal *= _moveLimiter;
+                _Vertical *= _moveLimiter;
+            }
+
+            rigidBody2D.velocity = new Vector2(_Horizontal * runSpeed, _Vertical * runSpeed);
+
+            ChangePlayerSprite();
         } else {
-            SetGunDirection(GunDirection.NONE);
-            animator.SetBool("IsRunning", false);
+            Destroy(this.gameObject);
         }
 
-        if (_Horizontal != 0 && _Vertical != 0) {
-            _Horizontal *= _moveLimiter;
-            _Vertical *= _moveLimiter;
-        }
-
-        rigidBody2D.velocity = new Vector2(_Horizontal * runSpeed, _Vertical * runSpeed);
-
-        ChangePlayerSprite();
     }
 
     private IEnumerator Dash() {
--- The Game/Assets/Scripts/Weapons/WeaponManager.cs
+++ The Game/Assets/Scripts/Weapons/WeaponManager.cs
@@ -8,7 +8,7 @@ public class WeaponManager : MonoBehaviour
 
     public GameObject[] OneHandedWeapos;
 
-    public Dictionary<string, GameObject> Weapons;
+    public List<GameObject> Weapons = new List<GameObject>();
 
     [SerializeField] public GameObject CurrentlyEquippedWeapon;
 
@@ -29,7 +29,26 @@ public class WeaponManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            if (CurrentlyEquippedWeapon != null) {
+                CurrentlyEquippedWeapon.SetActive(false);
+            }    
+            Weapons[0].SetActive(true);
+            CurrentlyEquippedWeapon = Weapons[0];
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            if (CurrentlyEquippedWeapon != null) {
+                CurrentlyEquippedWeapon.SetActive(false);
+            }
+            Weapons[1].SetActive(true);
+            CurrentlyEquippedWeapon = Weapons[1];
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            if (CurrentlyEquippedWeapon != null) {
+                CurrentlyEquippedWeapon.SetActive(false);
+            }
+            Weapons[2].SetActive(true);
+            CurrentlyEquippedWeapon = Weapons[2];
+        }
+
     }
 
     public void EquipOneHandedWeapon(GameObject weapon) {
